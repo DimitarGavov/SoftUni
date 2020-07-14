e1f1570c5144bf7cd85642a4bbd2a7fbe762b19c
MitkoePich <d.gavov@gmail.com>
2020-07-14 14:47:31 +0300

    1. Java Basic - Lab

---
+++ 01. Java Basics/First_Steps_In_Coding_Lab_01/Hello_SoftUni_01.java
@@ -0,0 +1,7 @@
+package First_Steps_In_Coding_Lab_01;
+
+public class Hello_SoftUni_01 {
+    public static void main(java.lang.String[] args) {
+        System.out.println("Hello SoftUni");
+    }
+}
---
+++ 01. Java Basics/First_Steps_In_Coding_Lab_01/Rectangle_Area_04.java
@@ -0,0 +1,14 @@
+package First_Steps_In_Coding_Lab_01;
+
+import java.util.Scanner;
+
+public class Rectangle_Area_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(a * b);
+    }
+}
