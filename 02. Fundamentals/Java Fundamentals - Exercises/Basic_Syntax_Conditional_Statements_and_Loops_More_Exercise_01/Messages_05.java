6a7c57c07b7bc7b6b63bcbcf44d910704dd7cbc4
MitkoePich <d.gavov@gmail.com>
2020-07-25 19:48:55 +0300

    33

---
+++ 02. Fundamentals/Java Fundamentals - Exercises/Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise_01/Messages_05.java
@@ -0,0 +1,11 @@
+package Basic_Syntax_Conditional_Statements_and_Loops_More_Exercise_01;
+
+import java.util.Scanner;
+
+public class Messages_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
